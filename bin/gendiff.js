5d1c5ea7a46f56b397497c1cc1ebe76e077197b8
Philipp <sabelnickov.filipp@yandex.ru>
2024-05-31 00:39:26 +0500

    add action for gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,26 @@
 #!/usr/bin/env node
 import { Command } from "commander"
-import { cwd } from 'node:process';
-import path from 'path';
-import fs from 'fs';
-const program = new Command();
+import parsing from "../modules/parsing.js";
 
-program
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0', '-V, --version', 'output the version number')
-    .option('-f, --format [type]', 'output format');
+const genDiff = () => {
+    const program = new Command();
 
-program.parse();
+    program
+        .argument('<filepath1>')
+        .argument('<filepath2>')
+        .description('Compares two configuration files and shows a difference.')
+        .version('1.0.0', '-V, --version', 'output the version number')
+        .option('-f, --format [type]', 'output format')
+        .action((filepath1, filepath2) => {
+            parsing(filepath1);
+            parsing(filepath2);
+        })
+
+    program.parse();
+
+    return program;
+};
+
+genDiff();
+
+export default genDiff;
