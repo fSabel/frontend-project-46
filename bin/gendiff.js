e15f46c6b2b66c5f88fbff770a7b85aa0bcac417
Philipp <sabelnickov.filipp@yandex.ru>
2025-07-09 23:31:57 +0500

    rec asciinema

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,7 +11,7 @@ const genDiff = () => {
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0', '-V, --version', 'output the version number')
     .option('-f, --format [type]', 'output format')
-    .action((filepath1, filepath2) => search(filepath1, filepath2));
+    .action((filepath1, filepath2) => console.log(search(filepath1, filepath2)));
 
   program.parse(process.argv);
 
