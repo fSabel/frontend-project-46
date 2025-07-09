15f3b3f1ee4474d446acaeb358c219ab93a9aaed
Philipp <sabelnickov.filipp@yandex.ru>
2025-07-09 22:53:03 +0500

    update gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,10 +9,14 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff', () => {
-  const pathFirstFile = getFixturePath('file1.json');
-  const pathSecondFile = getFixturePath('file2.json');
-  const expected = readFixtureFile('result.txt').trim();
+test.each([
+  ['file1.json', 'file2.json', 'resultJSON.txt'],
+  ['file1.yaml', 'file2.yml', 'resultYAML.txt'],
+  ['file1.json', 'file2.yml', 'resultYamlJson.txt'],
+])('gendiff', ([a, b, result]) => {
+  const pathFirstFile = getFixturePath(a);
+  const pathSecondFile = getFixturePath(b);
+  const expected = readFixtureFile(result).trim();
 
   expect(search(pathFirstFile, pathSecondFile)).toEqual(expected);
 });
