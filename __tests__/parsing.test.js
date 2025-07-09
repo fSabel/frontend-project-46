47424f581902fa4b33d4eb5e23096926f9ce1ea1
Philipp <sabelnickov.filipp@yandex.ru>
2025-07-09 22:53:14 +0500

    update parsing.test.js

--- __tests__/parsing.test.js
+++ __tests__/parsing.test.js
@@ -3,24 +3,21 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { expect, test } from '@jest/globals';
 import parsing from '../src/parsing.js';
+import formatObject from '../src/formatObject.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test.each([{ a: 'file1.json', b: 'file1.txt' }, { a: 'file2.json', b: 'file2.txt' }])('test parsing', ({ a, b }) => {
-  const formatObject = (obj) => {
-    const entries = Object.entries(obj)
-      .map(([key, value]) => {
-        const valStr = typeof value === 'string' ? `'${value}'` : value;
-        return `  ${key}: ${valStr}`;
-      })
-      .join(',\n');
-    return `{\n${entries}\n}`;
-  };
-  const parseFile = formatObject(parsing(a));
-  const result = readFixtureFile(b).trim();
+test.each([
+  { data: 'file1.json', expected: 'file1JSON.txt' },
+  { data: 'file2.json', expected: 'file2JSON.txt' },
+  { data: 'file1.yaml', expected: 'file1YAML.txt' },
+  { data: 'file2.yml', expected: 'file2YML.txt' },
+])('test parsing', ({ data, expected }) => {
+  const parseFile = formatObject(parsing(data));
+  const result = readFixtureFile(expected).trim();
 
   expect(parseFile).toEqual(result);
 });
