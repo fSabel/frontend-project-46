90a9f065a32bfca6161555516214b65c2f0339c5
Philipp <sabelnickov.filipp@yandex.ru>
2025-07-09 22:54:27 +0500

    update parsing.js

--- src/parsing.js
+++ src/parsing.js
@@ -6,12 +6,16 @@ import path from 'path';
 const parsing = (fileIsStr) => {
   const filePath = path.resolve(`${cwd()}`, '__fixtures__', `${fileIsStr}`);
   const fileFormat = path.extname(filePath).slice(1);
-  if (fileFormat === 'json') {
-    const fileIsJSON = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    return fileIsJSON;
+  switch (fileFormat) {
+  case 'json':
+    return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  case 'yaml':
+    return yaml.load(fs.readFileSync(filePath, 'utf8'));
+  case 'yml':
+    return yaml.load(fs.readFileSync(filePath, 'utf8'));
+  default:
+    return 'This file has an incorrect format.';
   }
-  const fileIsYAML = yaml.load(fs.readFileSync(filePath, 'utf8'));
-  return fileIsYAML;
 };
 
 export default parsing;
