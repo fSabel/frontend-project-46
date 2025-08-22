940db7bee61a3098e085fe6092fab861e2f70aec
fSabel <sabelnickov.filipp@yandex.ru>
2025-08-22 13:43:12 +0500

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,7 +12,7 @@ const cliProgram = () => {
     .arguments('<filepath1> <filepath2>')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0', '-V, --version', 'output the version number')
-    .option('-f, --format <type>', 'output format (default: stylish)')
+    .option('-f, --format <type>', 'output format', 'stylish')
     .action((filepath1, filepath2) => {
       const options = program.opts();
       console.log(genDiff(filepath1, filepath2, options.format));
